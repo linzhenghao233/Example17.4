079aed381f9b4fc4d8af15e1b272a55c7b0cf555
linzhenghao233 <1392872316@qq.com>
2023-09-22 22:32:26 +0800

    添加项目文件。

---
+++ films3.c
@@ -0,0 +1,11 @@
+/* films3.c -- 使用抽象数据类型（ADT）风格的链表 */
+/* 与list.c一起编译 */
+#include <stdio.h>
+#include <stdlib.h>
+#include "list.h"
+void showmovies(Item item);
+char* s_gets(char* st, int n);
+int main(void) {
+	list movies;
+	Item temp;
+}
