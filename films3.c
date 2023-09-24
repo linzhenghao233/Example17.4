29cc7938a6972f205b2eaa5931ab450d454c4d17
linzhenghao233 <1392872316@qq.com>
2023-09-24 21:23:43 +0800

    0924

--- films3.c
+++ films3.c
@@ -8,4 +8,14 @@ char* s_gets(char* st, int n);
 int main(void) {
 	list movies;
 	Item temp;
+
+	/* 初始化 */
+	InitializeList(&movies);
+	if (ListIsFull(&movies)) {
+		fprintf(stderr, "No memory available! Bye!\n");
+		exit(1);
+	}
+
+	/* 获取用户输入并储存 */
+	puts("Enter first movie title:");
 }
