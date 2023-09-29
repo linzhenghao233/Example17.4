429a3cf99ad02a397ed123618b1abb08a8ac37cd
linzhenghao233 <1392872316@qq.com>
2023-09-29 18:40:41 +0800

    0929

--- films3.c
+++ films3.c
@@ -18,4 +18,53 @@ int main(void) {
 
 	/* 获取用户输入并储存 */
 	puts("Enter first movie title:");
+	while (s_gets(temp.title, TSIZE) != NULL && temp.title[0] != '\0') {
+		puts("Enter your rating <0-10>:");
+		scanf_s("%d", &temp.rating);
+		while (getchar() != '\n')
+			continue;
+		if (AddItem(temp, &movies) == false) {
+			fprintf(stderr, "Problem allocating memory\n");
+			break;
+		}
+		if (ListIsFull(&movies)) {
+			puts("The list is now full.");
+			break;
+		}
+		puts("Enter next movie title (empty line to stop):");
+	}
+	/* 显示 */
+	if (ListIsEmpty(&movies))
+		printf("No data entered.");
+	else {
+		printf("Here is the movie list:\n");
+		Traverse(&movies, showmovies);
+	}
+	printf("You entered %d movies.\n", ListItemCount(&movies));
+
+	/* 清理 */
+	EmptyTheList(&movies);
+	printf("Bye!\n");
+
+	return 0;
+}
+
+void showmovies(Item item) {
+	printf("Movies: %s Rating: %d\n", item.title, item.rating);
+}
+
+char* s_gets(char* st, int n) {
+	char* ret_val;
+	char* find;
+
+	ret_val = fgets(st, n, stdin);
+	if (ret_val) {
+		find = strchr(st, '\n');
+		if (find)
+			*find = '\0';
+		else
+			while (getchar() != '\n')
+				continue;
+	}
+	return ret_val;
 }
